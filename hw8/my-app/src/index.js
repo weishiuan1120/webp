cfec2576b5ec25f4cdbccaa45cfc0658f20df08a
weishiuan1120 <melody19991120@gmail.com>
2022-05-20 01:42:03 +0800

    hw8

---
+++ hw8/my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginPage from './App';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <React.StrictMode>
+    
+      <LoginPage />
+   
+  </React.StrictMode>,
+  rootElement
+);
